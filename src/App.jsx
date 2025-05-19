258c9237f9a4d48fda69a20bd9a84618c23d07b2
elcbil <nabilahdaa@gmail.com>
2025-05-19 21:47:52 +0700

    Menambahkan route untuk halaman Tracker, Education, dan AboutUs beserta komponen terkait

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home'; 
+import Food from './pages/Food'; 
+import About from './pages/AboutUs';
+import Tracker from './pages/Tracker';  
+import Education from './pages/Education';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes> {}
-        <Route path="/" element={<Home />} /> {}
-        {}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/food" element={<Food />} /> 
+        <Route path="/aboutus" element={<About />} /> 
+        <Route path="/tracker" element={<Tracker />} />  
+        <Route path="/education" element={<Education />} />  
       </Routes>
       <Footer />
     </Router>
